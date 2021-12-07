1552b1ce9ff1b89e589dae22306658041c3e0f23
kekesibence <bence.kekesi@gmail.com>
2021-12-07 15:22:38 +0100

    feladat keszitese

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,11 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div id="app">
+   <HelloWorld msg="Welcome to Your Vue.js App"/>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  },
-  data() {
-    return {
-      rows: [
-        {
-          title: 'Kerék',
-          price: 100,
-          quantity: 12
-        },
-        {
-          title: 'Teleszkóp',
-          price: 1000,
-          quantity: 300
-        },
-        {
-          title: 'Kormány',
-          price: 230,
-          quantity: 5
-        },
-        {
-          title: 'Ajtó',
-          price: 45120,
-          quantity: 321
-        },
-      ]
-    }
-  }
-}
 </script>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -6,3 +6,30 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+var app = new Vue({
+  el: "app",
+  data: {
+    rows: [
+      {
+          title: 'Kerék',
+          price: 100,
+          quantity: 12
+        },
+        {
+          title: 'Teleszkóp',
+          price: 1000,
+          quantity: 300
+        },
+        {
+          title: 'Kormány',
+          price: 230,
+          quantity: 5
+        },
+        {
+          title: 'Ajtó',
+          price: 45120,
+          quantity: 321
+        }
+    ]
+  }});
