60849cb20cf9e07206b4e7c658e0857fd5572014
kekesibence <bence.kekesi@gmail.com>
2021-12-07 15:37:19 +0100

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,44 @@
 <template>
 <div id="app">
-   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </div>
 </template>
 
 <script>
+import HelloWorld from './components/HelloWorld.vue'
 
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      data: {
+      rows: [
+        {
+            title: 'Kerék',
+            price: 100,
+            quantity: 12
+          },
+          {
+            title: 'Teleszkóp',
+            price: 1000,
+            quantity: 300
+          },
+          {
+            title: 'Kormány',
+            price: 230,
+            quantity: 5
+          },
+          {
+            title: 'Ajtó',
+            price: 45120,
+            quantity: 321
+          }
+      ]
+    }
+  }
+}}
 </script>
 
 <style>
